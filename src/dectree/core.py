a4517b4d39c8fe76049301336526a7dd1a1bb5b0
Morteza-Khazaei <mortezakhazaei1370@gmail.com>
2023-09-01 13:14:45 +0200

    First version

---
+++ src/dectree/core.py
@@ -0,0 +1,345 @@
+import os
+import sys
+import json
+import argparse
+import zipfile
+import logging
+import requests
+import jdatetime
+import numpy as np
+from osgeo import gdal, ogr
+
+
+
+
+
+
+
+
+class DecTree:
+
+
+    def __init__(self, address:str, username:str, password:str, input:str, output:str, 
+                 landcover:str, false_mask:str, seed_db:bool, **kwargs) -> None:
+
+        self.input_base_dir = input
+        self.output_base_dir = output
+        
+        # datebase connection params
+        self.seed_db = seed_db
+        self.username = username
+        self.password = password
+        self.address = address
+        
+        self.lc_ds = gdal.Open(landcover, gdal.GA_ReadOnly)
+        # Get the first input raster band
+        self.lc_band = self.lc_ds.GetRasterBand(1)
+        # Get raster bbox
+        self.lc_bbox = self.__getBBox(self.lc_ds)
+        # The inverse geotransform is used to convert lon/lat degrees to x/y pixel index
+        lc_geotrans = self.lc_ds.GetGeoTransform()
+        self.lc_inv_geotrans = gdal.InvGeoTransform(lc_geotrans)
+
+        # Open input raster by gdal
+        fm_ds = gdal.Open(false_mask, gdal.GA_ReadOnly)
+
+        # Get the first input raster band
+        self.fm_band = fm_ds.GetRasterBand(1)
+
+        return None
+
+
+    @staticmethod
+    def init_loggers(msg_level=logging.DEBUG):
+        """
+        Init a stdout logger
+        :param msg_level: Standard msgLevel for both loggers. Default is DEBUG
+        """
+
+        logging.getLogger().addHandler(logging.NullHandler())
+        # Create default path or get the pathname without the extension, if there is one
+        dectree_logger = logging.getLogger("root")
+        dectree_logger.handlers = []  # Remove the standard handler again - Bug in logging module
+
+        dectree_logger.setLevel(msg_level)
+        formatter = logging.Formatter("%(asctime)s [%(levelname)-5.5s] %(message)s")
+
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setFormatter(formatter)
+        dectree_logger.addHandler(console_handler)
+        
+        return dectree_logger
+
+
+    def __getBBox(self, ds):
+        """
+        Get raster dataset and create bbox for it
+        :param ds: gdal dataset
+        :return: bbox
+        """
+        xmin, xpixel, _, ymax, _, ypixel = ds.GetGeoTransform()
+        width, height = ds.RasterXSize, ds.RasterYSize
+        xmax = xmin + width * xpixel
+        ymin = ymax + height * ypixel
+
+        raster_bounds = ogr.Geometry(ogr.wkbLinearRing)
+        raster_bounds.AddPoint(xmin, ymin)
+        raster_bounds.AddPoint(xmax, ymin)
+        raster_bounds.AddPoint(xmax, ymax)
+        raster_bounds.AddPoint(xmin, ymax)
+        raster_bounds.AddPoint(xmin, ymin)
+        bbox = ogr.Geometry(ogr.wkbPolygon)
+        bbox.AddGeometry(raster_bounds)
+
+        return bbox
+
+
+    def __get_token(self, username, password):
+        token_url = "http://192.168.66.66:9090/gcms/accounts/api/auth/login"
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+        auth_data = {
+            "email": username,
+            "password": password
+        }
+        resp = requests.post(token_url, data=json.dumps(auth_data), headers=headers).json()
+
+        return resp["token"]
+    
+
+    def __process_chmap(self, chmap:str, bin_file_path:str):
+        
+        # trg_ds = gdal.Open(chmap, gdal.GA_ReadOnly)
+        trg_ds = gdal.Warp('', chmap, dstSRS='EPSG:3857', format='MEM', xRes=10, yRes=10)
+
+        trg_geoTrans = trg_ds.GetGeoTransform()
+
+        trg_bands = trg_ds.RasterCount        # Number of bands
+        trg_projection = trg_ds.GetProjection()      # Projection
+
+        # Get raster bbox
+        trg_bbox = self.__getBBox(trg_ds)
+
+        # Get intersection between two geometry
+        intersection = self.lc_bbox.Intersection(trg_bbox)
+
+        # Check if two geom have intersection
+        if intersection is not None and intersection.Area() > 0:
+
+            # Get bound of overlap
+            bounds_geo = intersection.Boundary()
+
+            # Get extent of input raster
+            xmin_sub, xmax_sub, ymin_sub, ymax_sub = bounds_geo.GetEnvelope()
+
+            # Create a new geomatrix for the image
+            new_trg_geoTrans = list(trg_geoTrans)
+            new_trg_geoTrans[0] = xmin_sub
+            new_trg_geoTrans[3] = ymax_sub
+
+            # The inverse geotransform is used to convert lon/lat degrees to x/y pixel index
+            trg_inv_geotrans = gdal.InvGeoTransform(trg_geoTrans)
+
+            # Convert lon/lat degrees to x/y pixel for the dataset
+            ulX_sub, ulY_sub = gdal.ApplyGeoTransform(trg_inv_geotrans, xmin_sub, ymax_sub)
+            lrX_sub, lrY_sub = gdal.ApplyGeoTransform(trg_inv_geotrans, xmax_sub, ymin_sub)
+
+            xsize_sub = int(lrX_sub - ulX_sub)
+            ysize_sub = int(lrY_sub - ulY_sub)
+
+            image_bands = []
+            for b in range(trg_bands):
+                image_bands.append(trg_ds.GetRasterBand(b + 1).ReadAsArray(int(ulX_sub), int(ulY_sub), xsize_sub, ysize_sub))
+
+            blue_band, green_band, red_band, nir_band, kisqr_band = image_bands
+
+            # All changes strong
+            total_change_strong = np.logical_and(
+                np.logical_and(
+                    np.logical_and(blue_band < 10.0, blue_band > 2.0),
+                    np.logical_and(red_band < -1.0, red_band > -5.0)),
+                np.logical_and(
+                    np.logical_and(nir_band < -1.0, nir_band > -5.0),
+                    np.logical_and(kisqr_band < 1500, kisqr_band > 150))
+            )
+
+            # All changes weak
+            total_change_weak = np.logical_and(
+                np.logical_and(
+                    np.logical_and(blue_band < 11.0, blue_band > 1.0),
+                    np.logical_and(red_band < -0.0, red_band > -6.0)),
+                np.logical_and(
+                    np.logical_and(nir_band < -0.0, nir_band > -6.0),
+                    np.logical_and(kisqr_band < 2000, kisqr_band > 100))
+            )
+
+            # No data mask
+            nodata_mask = kisqr_band >= 2000
+
+            # Convert lon/lat degrees to x/y pixel for the dataset
+            ulX, ulY = gdal.ApplyGeoTransform(self.lc_inv_geotrans, xmin_sub, ymax_sub)
+            lrX, lrY = gdal.ApplyGeoTransform(self.lc_inv_geotrans, xmax_sub, ymin_sub)
+
+            xsize = int(lrX - ulX)
+            ysize = int(lrY - ulY)
+
+            # Get subset of the raster as a numpy array
+            lc_sub_array = self.lc_band.ReadAsArray(int(ulX), int(ulY), xsize, ysize)
+
+            # Mask out other classes
+            other_classes = np.isin(lc_sub_array, [2, 3, 4, 5, 6])
+            
+            # Get subset of the raster as a numpy array
+            src_sub_array_fchm = self.fm_band.ReadAsArray(int(ulX), int(ulY), xsize, ysize)
+            mask_fchm = src_sub_array_fchm == 1
+
+            # Mask out unchanged pixels strong
+            total_change_strong[other_classes] = 0
+            total_change_strong[nodata_mask] = 0
+
+            # Mask out unchanged pixels weak
+            total_change_weak[other_classes] = 0
+            total_change_weak[nodata_mask] = 0
+
+            sum_change = np.add(total_change_strong, total_change_weak, dtype=int)
+
+            # Write the output into geotiff image.
+            sum_fname = bin_file_path.replace('BIN', 'SUM')
+            drv = gdal.GetDriverByName('GTiff')
+            sum_ds = drv.Create(sum_fname, xsize, ysize, 1, gdal.GDT_Byte, options=['COMPRESS=LZW'])
+            sum_band = sum_ds.GetRasterBand(1)
+            sum_band.WriteArray(sum_change)
+
+            prx_fname = bin_file_path.replace('BIN', 'PROX')
+            prx_ds = drv.Create(prx_fname, xsize, ysize, 1, gdal.GDT_Byte, options=['COMPRESS=LZW'])
+            prx_band = prx_ds.GetRasterBand(1)
+
+            gdal.ComputeProximity(sum_band, prx_band,
+                    options=["VALUES=2", "MAXDIST=5", "DISTUNITS=PIXEL", "NODATA=255", "FIXED_BUF_VAL=0"], callback=None) #gdal.TermProgress
+
+            prx_array = prx_band.ReadAsArray()
+
+            total_change = np.logical_and(total_change_weak, prx_array==0)
+
+            # Forest changes
+            forest_changes = np.logical_and(total_change, lc_sub_array==0)
+
+            # Rangeland changes
+            rangeland_changes = np.logical_and(total_change, lc_sub_array==1)
+
+            # Assign class labels
+            final_array = np.full(lc_sub_array.shape, 255, dtype=int)
+            final_array[forest_changes] = 0
+            final_array[rangeland_changes] = 1
+            
+            final_array[mask_fchm] = 255
+
+            driver = gdal.GetDriverByName('GTiff')
+            bin_ds = driver.Create(bin_file_path, xsize, ysize, 1, gdal.GDT_Byte, options=['COMPRESS=LZW'])
+            bin_ds.SetGeoTransform(new_trg_geoTrans)
+
+            # Create for target raster the same projection as for the value raster
+            bin_ds.SetProjection(trg_projection)
+
+            bin_band = bin_ds.GetRasterBand(1)
+            bin_band.WriteArray(final_array)
+
+            bin_band.GetRasterBand(1).SetNoDataValue(255)
+
+            bin_band.FlushCache()
+        
+        return None
+
+
+    def __db_seeder(self, url, headers, image_path):
+
+        out_dir, fname = os.path.splitext(image_path)
+
+        platform, date_obj, product, tile, c, version, ftype = fname.split('_')
+        date_time_str = date_obj.split('-')[0]
+
+        jalali_date =  jdatetime.datetime.strptime(date_time_str, '%Y%m%d')
+
+        yyyymm = jalali_date.strftime("%Y%m")
+
+        pname = ''.join(['_'.join([ftype, yyyymm, tile]), '.tif']) #CHMAP_139802_39SUB.tif
+
+        zfname = ''.join(['_'.join([ftype, yyyymm, tile]), '.zip']) #CHMAP_139802_39SUB.zip
+
+        zf = zipfile.ZipFile(os.path.join(out_dir, zfname), "w", zipfile.ZIP_DEFLATED)
+
+        zf.write(out_dir, pname)
+        zf.close()
+
+        data = {
+            'year': jalali_date.year,
+            'month': jalali_date.month,
+            'scene_name': tile
+        }
+
+        files = {'zip_file': open(os.path.join(out_dir, zfname), 'rb')}
+
+        resp =  requests.post(url, data=data, headers=headers, files=files)
+        print(resp.status_code)
+        print(resp.json())
+
+
+    def run(self):
+        tiles = os.listdir(self.input_base_dir)
+
+        for tile in tiles:
+            out_dir = os.path.join(self.output_base_dir, tile)
+
+            if not os.path.exists(out_dir):
+                os.makedirs(out_dir)
+
+            chmap_paths = os.path.join(self.input_base_dir, tile)
+
+            chmaps = os.listdir(chmap_paths)
+            for file in chmaps:
+                chmap_file_path = os.path.join(chmap_paths, file)
+
+                bname = file.replace('CHMAP', 'BIN')
+                bin_file_path = os.path.join(out_dir, bname)
+
+                if not os.path.exists(bin_file_path):
+                    print('Create file %s' % bin_file_path)
+
+                    self.__process_chmap(chmap_file_path, bin_file_path)
+
+                    if self.seed_db:
+                        url = f'{self.address}/gcms/api/TreeCoverLossRaster/'
+                        auth_token = self.__get_token(self.username , self.password)
+                        headers = {'Accept': 'application/json', 'Authorization': 'Token {}'.format(auth_token)}
+
+                        nrgb_name = file.replace('CHMAP', 'NRGB')
+                        nrgb_file_path = os.path.join(out_dir.replace('CHMAP', 'L3A'), nrgb_name)
+                        
+                        self.__db_seeder(url, headers, nrgb_file_path)
+                        self.__db_seeder(url, headers, bin_file_path)
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Perfrom IR-MAD change detection on bitemporal, multispectral imagery.")
+
+    parser.add_argument("-a", "--address", type=str, help="WebApp address")
+    parser.add_argument("-u", "--username", type=str, help="WebApp username")
+    parser.add_argument("-p", "--password", type=str, help="WebApp password")
+    parser.add_argument("-i", "--input", type=str, help="Where did you store L3A products?")
+    parser.add_argument("-o", "--output", type=str, help="Where is your preferred direction to store change maps?")
+    parser.add_argument("-l", "--landcover", type=str, help="Landcover map")
+    parser.add_argument("-m", "--false_mask", type=str, help="False mask map")
+    parser.add_argument("-s", "--seed_db", type=bool, help="Upload output to the WebApp database", default=True)
+    parser.add_argument("-v", "--verbose", help="Provides detailed (DEBUG) logging for DecTree. Default is false",
+                        default=False, action="store_true")
+
+    args = parser.parse_args()
+
+    # TODO Add error skipping
+    logging_level = logging.DEBUG if args.verbose else logging.INFO
+    logger = DecTree.init_loggers(msg_level=logging_level)
+
+
+    tree = DecTree(args.address, args.username, args.password, args.input, args.output, 
+                   args.landcover, args.false_mask, logger=logger)
+    tree.run()
